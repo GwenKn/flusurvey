32d8ad3d68b37c826200907f198c92f83cfa43a1
GwenKn <g.knight@imperial.ac.uk>
2018-02-28 17:54:56 +0000

    need to make adaptable to more than one category

--- R/odds_ratios.R
+++ R/odds_ratios.R
@@ -9,20 +9,34 @@ plots <- "~/Documents/Flusurvey/plots/"
 datap <- "~/Documents/Flusurvey/data/"
 
 ### functions
-orwp.ma <- function(n00, n01, n10, n11, alpha = 0.05){
+orwp.ma <- function(datas,category, base){
   #
-  #  Compute the odds ratio between two binary variables, x and y,
-  #  as defined by the four numbers nij:
-  #
+  #  Compute the odds ratio between category and medication.antibiotic
+  
+  if (base == 0){ #if only 2 levels 
+    w <- which(colnames(datas) == category)
+    tt <- table(datas[,w], datas$medication.antibiotic)
+  } else {
+    
+    
+  }
+  
+  
   #    n00 = number of cases where x = 0 and y = 0 ## x = medication.antibiotic 
   #    n01 = number of cases where x = 0 and y = 1
   #    n10 = number of cases where x = 1 and y = 0
   #    n11 = number of cases where x = 1 and y = 1
+  n00 <- tt[1] 
+  n01 <- tt[2]  
+  n10 <- tt[3] 
+  n11 <- tt[4] 
+  
   #
   OR <- (n00 * n11)/(n01 * n10)
   #
   #  Compute the Wald confidence intervals:
   #
+  alpha = 0.05 # 95% CI
   siglog <- sqrt((1/n00) + (1/n01) + (1/n10) + (1/n11))
   zalph <- qnorm(1 - alpha/2)
   logOR <- log(OR)
@@ -36,8 +50,9 @@ orwp.ma <- function(n00, n01, n10, n11, alpha = 0.05){
   oframe
 }
 
-orwp(3920,288,1692,2224,alpha = 0.05)
+orwp.ma(3920,288,1692,2224,alpha = 0.05)
 
+orwp.ma(dabx,"gender",0)
 
 ### data
 setwd(datap)
